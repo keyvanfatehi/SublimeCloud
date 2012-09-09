3d5b203b1f0e3e4731420c5729532a4824823e06
Keyvan Fatehi <keyvanfatehi@gmail.com>
2012-09-09 16:53:22 -0700

    Grabbing the git URI

--- SublimeCloud.py
+++ SublimeCloud.py
@@ -1,25 +1,11 @@
 import sublime, sublime_plugin
 
-from urllib import urlopen
-import json
-
-class SublimeCloudSettings
-  def __init__(self)
-    settings_file = open(os.path.expanduser('~/.sublime_cloud'))
-    data = json.load(json_data)
-    settings_file.close()
-    self.repo = data["github_repository"]
-
 class ExampleCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    self.view.insert(edit, 0, "Hello, World!")
-
-class GoogleAutocomplete(sublime_plugin.EventListener):
-    def on_query_completions(self, view, prefix, locations):
-        elements = ET.parse(
-                        urlopen(GOOGLE_AC % prefix)
-                    ).getroot().findall("./CompleteSuggestion/suggestion")
-
-        sugs = [(x.attrib["data"],) * 2 for x in elements]
-
-        return sugs
+    settings = self.view.settings()
+    key = "sublime_cloud_git"
+    if settings.has(key):
+        output = settings.get(key)
+    else:
+        output = 'Add "sublime_cloud_git" to your settings!'
+    self.view.insert(edit, 0, output)
